0c3e5020e87c63b467fdba8a04adef4bf3804b1b
Priyanka Khandge <priyakhandge10@gmail.com>
2020-09-23 15:56:41 +0530

    added print string from user

---
+++ src/PrintString.kt
@@ -0,0 +1,9 @@
+
+fun main(args: Array<String>){
+    val name1: String = "kotlin"
+    println(name1)
+    
+    println("Enter the name:")
+    val name = readLine()
+    println("your name is: $name")
+}
