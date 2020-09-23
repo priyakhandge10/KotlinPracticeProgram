53f8af5f1438dc443748b8e815a864fb8b66e9a2
Priyanka Khandge <priyakhandge10@gmail.com>
2020-09-23 15:57:16 +0530

    added integer number

---
+++ src/PrintIntegerNumber.kt
@@ -0,0 +1,11 @@
+import java.util.*
+
+fun main(args: Array<String>){
+    val reader: Scanner = Scanner(System.`in`)
+    println("Enter the number:")
+    val integer:Int = reader.nextInt()
+    println("Enter the number:")
+    val long: Long = reader.nextLong()
+    println("Your number is: $integer")
+    println("Your long number is: $long ")
+}
