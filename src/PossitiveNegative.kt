c06a62f324ea4a6de9c5ff8add40a46606c5f91b
Priyanka Khandge <priyakhandge10@gmail.com>
2020-09-24 18:26:24 +0530

    added find positive negative number

---
+++ src/PossitiveNegative.kt
@@ -0,0 +1,16 @@
+import java.util.*
+
+fun main(args: Array<String>){
+    println("enter the number:")
+    val reader =  Scanner(System.`in`)
+    val number = reader.nextFloat()
+    when {
+        number > 0.0 -> {
+            println("$number is positive")
+        }
+        number < 0.0 -> {
+            println("$number is negative")
+        }
+        else -> println("$number is 0")
+    }
+}
